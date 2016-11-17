b6f9a3bf9b4fddc273cf0af994d76957447c6954
Nicholas Vanhaute <njvanhaute@crimson.ua.edu>
2016-11-16 19:18:18 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MandelbrotGenerator
+//
+//  Created by Nicholas Vanhaute on 11/16/16.
+//  Copyright © 2016 Nicholas Vanhaute. All rights reserved.
+//
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, const char *argv[]) {
+    
+    return 0;
+}
